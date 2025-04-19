8023b38aeaedc5d20ddda160da4aa8f3fc297881
Yakup Unyilmaz <yakup.unyilmaz@mendix.com>
2025-04-19 03:04:36 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Yakup Ünyilmaz on 19/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Yakup Ünyilmaz on 19/04/2025.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
